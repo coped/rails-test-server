d45a1c04123eca0e653d8eae9c73f0fc1ed15182
Dennis Cope <dennisaaroncope@gmail.com>
2019-03-18 15:06:43 -0700

    Made changes to the REAME file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     def hello
-        render html: "¡Hola, mundo!"
-    end
-    def goodbye
-        render html: "Goodbye World!"
+        render html: "Hello World!"
     end
 end
